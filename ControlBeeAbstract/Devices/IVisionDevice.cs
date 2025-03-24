861f9612ebfafe0d14d948cb2377a3e43043d2ea
Leo <leo@hotbee.co>
2025-03-24 10:35:20 +0900

    GetResult in Dict

--- ControlBeeAbstract/Devices/IVisionDevice.cs
+++ ControlBeeAbstract/Devices/IVisionDevice.cs
@@ -1,8 +1,10 @@
 ﻿namespace ControlBeeAbstract.Devices;
 
+using Dict = Dictionary<string, object?>;
+
 public interface IVisionDevice : IDevice
 {
     void Trigger(int channel);
     void Wait(int channel, int timeout);
-    string GetResult(int channel);
+    Dict GetResult(int channel);
 }
