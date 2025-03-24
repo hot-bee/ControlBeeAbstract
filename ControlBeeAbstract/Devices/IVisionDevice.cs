6e08d93926a5779fe4ddd960e7cbf3564c3ed366
Leo <leo@hotbee.co>
2025-03-24 09:34:06 +0900

    SC-626 Add Vision Device

---
+++ ControlBeeAbstract/Devices/IVisionDevice.cs
@@ -0,0 +1,8 @@
+﻿namespace ControlBeeAbstract.Devices;
+
+public interface IVisionDevice : IDevice
+{
+    void Trigger(int channel);
+    void Wait(int channel, int timeout);
+    string GetResult(int channel);
+}
