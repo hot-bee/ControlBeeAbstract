9e992ae122c73d9d180857910082387d818fcacb
Leo <leo@hotbee.co>
2025-03-26 19:46:14 +0900

    SC-645 Introduce ECamTable

--- ControlBeeAbstract/Devices/IMotionDevice.cs
+++ ControlBeeAbstract/Devices/IMotionDevice.cs
@@ -4,6 +4,7 @@ public interface IMotionDevice : IDevice
 {
     void Enable(int channel, bool value);
     bool IsEnabled(int channel);
+
     void TrapezoidalMove(
         int channel,
         int position,
@@ -11,9 +12,21 @@ public interface IMotionDevice : IDevice
         int acceleration,
         int deceleration
     );
+
     void Wait(int channel);
     void Wait(int channel, int timeout);
     bool IsMoving(int channel);
     void SetCommandPosition(double position);
     void SetActualPosition(double position);
+
+    void StartECam(
+        int tableIndex,
+        int masterChannel,
+        int slaveChannel,
+        double[] masterPositions,
+        double[] slavePositions
+    );
+
+    void StopECam(int tableIndex);
+    bool IsECamEnabled(int tableIndex);
 }
