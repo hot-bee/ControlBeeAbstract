9ab8f3e7a148001aac9c50382c850115c6158af5
Leo <leo@hotbee.co>
2025-05-24 09:32:00 +0900

    Introduce IUserMemoryDevice

---
+++ ControlBeeAbstract/Devices/IUserMemoryDevice.cs
@@ -0,0 +1,19 @@
+﻿namespace ControlBeeAbstract.Devices;
+
+public interface IUserMemoryDevice : IDevice
+{
+    void SetUserMemoryBit(int channel, int offset, byte value);
+    void SetUserMemoryByte(int channel, byte value);
+    void SetUserMemorySignedByte(int channel, sbyte value);
+    void SetUserMemoryWord(int channel, ushort value);
+    void SetUserMemorySignedWord(int channel, short value);
+    void SetUserMemoryDWord(int channel, uint value);
+    void SetUserMemorySignedDWord(int channel, int value);
+    byte GetUserMemoryBit(int channel, int offset);
+    byte GetUserMemoryByte(int channel);
+    sbyte GetUserMemorySignedByte(int channel);
+    ushort GetUserMemoryWord(int channel);
+    short GetUserMemorySignedWord(int channel);
+    uint GetUserMemoryDWord(int channel);
+    int GetUserMemorySignedDWord(int channel);
+}
