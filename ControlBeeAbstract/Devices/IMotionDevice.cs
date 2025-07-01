afaf5bc2d874641da4520336b3911346d2928a56
Leo Kim <leo@hotbee.co>
2025-07-01 11:39:23 +0900

    Add interpolation move function

--- ControlBeeAbstract/Devices/IMotionDevice.cs
+++ ControlBeeAbstract/Devices/IMotionDevice.cs
@@ -20,6 +20,9 @@ public interface IMotionDevice : IDevice
     void JerkRatioSCurveMove(int channel, double position, double velocity, double acceleration,
         double deceleration, double accelJerkRatio, double decelJerkRatio);
 
+    void JerkRatioSCurveMove((int channel, double position)[] channelAndPositions, double velocity, double acceleration,
+        double deceleration, double accelJerkRatio, double decelJerkRatio);
+
     void Wait(int channel);
     void Wait(int channel, int timeout);
     bool IsMoving(int channel);
