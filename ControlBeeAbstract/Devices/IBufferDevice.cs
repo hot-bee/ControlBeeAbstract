3a3cae249dc21c0a615445c4c829d835257df41b
Leo Kim <leo@hotbee.co>
2025-07-24 23:08:32 +0900

    Introduce IBufferDevice

---
+++ ControlBeeAbstract/Devices/IBufferDevice.cs
@@ -0,0 +1,13 @@
+﻿namespace ControlBeeAbstract.Devices;
+
+public interface IBufferDevice : IDevice
+{
+    void RunBuffer(int bufferIndex, string label);
+    void StopBuffer(int bufferIndex);
+
+    object ReadVariable(string variable, int bufferIndex = -1,
+        int from1 = -1, int to1 = -1, int from2 = -1, int to2 = -1);
+
+    void WriteVariable(object value, string variable, int bufferIndex = -1,
+        int from1 = -1, int to1 = -1, int from2 = -1, int to2 = -1);
+}
