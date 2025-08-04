f92c8d4b05c895bccd6662b0da5a63eec7635785
soon <35055716+tnsnz@users.noreply.github.com>
2025-08-04 10:43:51 +0900

    Add IsRunningBuffer function to IBufferDevice (#1)
    Co-authored-by: soon <tnsnz@github.com>

--- ControlBeeAbstract/Devices/IBufferDevice.cs
+++ ControlBeeAbstract/Devices/IBufferDevice.cs
@@ -5,6 +5,8 @@ public interface IBufferDevice : IDevice
     void RunBuffer(int bufferIndex, string label);
     void StopBuffer(int bufferIndex);
 
+    bool IsRunningBuffer(int bufferIndex);
+
     object ReadVariable(string variable, int bufferIndex = -1,
         int from1 = -1, int to1 = -1, int from2 = -1, int to2 = -1);
 
