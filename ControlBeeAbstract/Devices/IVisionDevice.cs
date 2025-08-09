0fd7355e10361b720fd07d811c57f113c22cf66f
Leo <leo@hotbee.co>
2025-08-09 19:16:05 +0900

    Embed Vision with channel

--- ControlBeeAbstract/Devices/IVisionDevice.cs
+++ ControlBeeAbstract/Devices/IVisionDevice.cs
@@ -10,7 +10,7 @@ public interface IVisionDevice : IDevice
     void Wait(int channel, int inspectionIndex, int timeout);
     void WaitGrabEnd(int channel, int inspectionIndex, int timeout);
     JsonObject GetResult(int channel, int inspectionIndex);
-    void EmbedVisionView(IntPtr parentHandle);
+    void EmbedVisionView(IntPtr parentHandle, int channel);
     void StartContinuous(int channel);
     void StopContinuous(int channel);
     void FocusChannel(int channel);
