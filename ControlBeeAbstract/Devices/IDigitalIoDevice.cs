624ca12e7cecb7e0c46d7e9286f653b1c051c111
Leo Kim <leo@hotbee.co>
2025-12-08 09:31:05 +0900

    SetDigitalInputBitInverted

--- ControlBeeAbstract/Devices/IDigitalIoDevice.cs
+++ ControlBeeAbstract/Devices/IDigitalIoDevice.cs
@@ -6,4 +6,6 @@ public interface IDigitalIoDevice : IDevice
     void SetDigitalOutputBit(int channel, bool value);
     bool GetDigitalOutputBit(int channel);
     event EventHandler<(int channel, bool value)>? OutputBitChanged;
+    bool SetDigitalInputBitInverted(int channel, bool inverted);
+    bool GetDigitalInputBitInverted(int channel);
 }
