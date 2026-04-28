141d5569e73963200f82ceb6494bb69f259a6364
kangyunkim <rkddbs0511@naver.com>
2026-04-28 14:02:02 +0900

    Add Reconnected event to ICounterDevice

--- ControlBeeAbstract/Devices/ICounterDevice.cs
+++ ControlBeeAbstract/Devices/ICounterDevice.cs
@@ -7,4 +7,5 @@ public interface ICounterDevice : IDevice
     void SetEncoderMode(int channel, EncoderMode mode);
     void SetCounterValue(int channel, double value);
     double GetCounterValue(int channel);
+    event EventHandler? Reconnected;
 }
